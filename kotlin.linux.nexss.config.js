ce5a4e8929d920594dc2fbc359ec63115224b105
Marcin Polak <mapoart@gmail.com>
2020-09-23 18:20:39 +0200

    Kotlin for Arch and reset

--- kotlin.linux.nexss.config.js
+++ kotlin.linux.nexss.config.js
@@ -1,45 +1,32 @@
 let languageConfig = Object.assign({}, require("./kotlin.win32.nexss.config"));
 
+const os = require("@nexssp/os");
+const sudo = os.sudo();
+
+const distName = os.name();
+languageConfig.dist = distName;
+
 languageConfig.compilers = {
   kotlin: {
-    install: "apt install openjdk-11-jdk && snap install --classic kotlin",
+    install: `${sudo}apt install -y kotlin`,
     command: "kotlinc",
     args: "-script <file> --",
     help: ``,
   },
 };
 
-languageConfig.languagePackageManagers = {
-  gradle: {
-    // TODO:
-    // Gradle cheatsheet https://gist.github.com/jahe/59557d507f43574b0d96
-    installation: "scoop install gradle",
-    messageAfterInstallation: null, // sometimes there is need of add something to the files can be add here eg php for composer.
-    build: "gradle build",
-    tasks: "gradle tasks",
-    search: "gradle search",
-    install: "gradle install",
-    uninstall: "gradle remove",
-    help: "gradle help",
-    version: "gradle --version",
-    init: () => {
-      if (
-        !require("fs").existsSync(
-          require("path").join(process.cwd(), ".gradle")
-        )
-      ) {
-        require("child_process").execSync("gradle init", {
-          stdio: "inherit",
-        });
-        console.log("initialized kotlin gradle project.");
-      } else {
-        console.log("kotlin gradle already initialized.");
-      }
-    },
-    // if command not found in specification
-    // run directly on package manager
-    else: "gradle",
-  },
-};
+switch (distName) {
+  case os.distros.ARCH:
+    languageConfig.compilers.kotlin.install = `${sudo}pacman -S --noconfirm kotlin`; // error: package org.json does not exist
+    break;
+  case os.distros.UBUNTU:
+    languageConfig.compilers.kotlin.install = `${sudo}apt install openjdk-11-jdk && snap install --classic kotlin`;
+    break;
+  default:
+    languageConfig.compilers.kotlin.install = os.replacePMByDistro(
+      languageConfig.compilers.java8.install
+    );
+    break;
+}
 
 module.exports = languageConfig;
