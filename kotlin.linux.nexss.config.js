2e4b6362b4ec77972953c64fa2429a0eb23684a0
Marcin Polak <mapoart@gmail.com>
2021-04-26 15:38:53 +0200

    remove dependency @nexssp/os and use nexss programmer's one

--- kotlin.linux.nexss.config.js
+++ kotlin.linux.nexss.config.js
@@ -1,11 +1,8 @@
 let languageConfig = Object.assign({}, require("./kotlin.win32.nexss.config"));
 
-// const os = require("@nexssp/os")
-// Load os from Nexss CLI path (it can be changed if needed)
-const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
-const sudo = os.sudo();
+const sudo = process.sudo;
 
-const distName = os.name();
+const distName = process.distro;
 languageConfig.dist = distName;
 
 languageConfig.compilers = {
@@ -18,30 +15,30 @@ languageConfig.compilers = {
 };
 
 switch (distName) {
-  case os.distros.CENTOS:
+  case process.distros.CENTOS:
     languageConfig.compilers.kotlin.install = `${sudo}yum install epel-release
 ${sudo}yum install -y snapd
 ${sudo}systemctl enable --now snapd.socket
 ${sudo}ln -s /var/lib/snapd/snap /snap
 ${sudo}snap install kotlin --classic`;
     break;
-  case os.distros.FEDORA:
+  case process.distros.FEDORA:
     languageConfig.compilers.kotlin.install = `${sudo}dnf install -y snapd
 ${sudo}ln -s /var/lib/snapd/snap /snap
 ${sudo}snap install kotlin --classic`;
     break;
-  case os.distros.DEBIAN:
+  case process.distros.DEBIAN:
     languageConfig.compilers.kotlin.install = `${sudo}apt update
 ${sudo}apt install -y snapd
 ${sudo}snap install kotlin --classic`;
     break;
-  case os.distros.AMAZON:
+  case process.distros.AMAZON:
     languageConfig.compilers.kotlin.install = `${sudo}yum install -y kotlin`; // error: package org.json does not exist
     break;
-  case os.distros.ARCH:
+  case process.distros.ARCH:
     languageConfig.compilers.kotlin.install = `${sudo}pacman -S --noconfirm kotlin`; // error: package org.json does not exist
     break;
-  case os.distros.UBUNTU:
+  case process.distros.UBUNTU:
     languageConfig.compilers.kotlin.install = `${sudo}apt install -y openjdk-11-jdk && snap install --classic kotlin`;
     break;
   default:
